ccf592e12f73e72fc48b761c1a4cb5c3503addb9
spjustice <sanyok.vergun@gmail.com>
2024-12-26 17:43:51 +0400

    add contracts

--- js/app.js
+++ js/app.js
@@ -4027,8 +4027,9 @@
         updateOnBlur: true,
         weekStart: 0
     };
-    var range = document.createRange();
+    var range = null;
     function parseHTML(html) {
+        if (range == null) range = document.createRange();
         return range.createContextualFragment(html);
     }
     function hideElement(el) {
