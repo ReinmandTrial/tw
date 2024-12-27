6d91419cb0280e737e52d377327640ce2ca7716e
spjustice <sanyok.vergun@gmail.com>
2024-12-27 13:58:20 +0400

    sidebar fixes

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -49,7 +49,7 @@ var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
+    themeToggleLightIcon?.classList?.remove('hidden');
 } else {
     themeToggleDarkIcon?.classList?.remove('hidden');
 }
